7274bcda9984b5ffd8768976e669809d55f7f523
Camila Lourenço <61992484+camilalourencos@users.noreply.github.com>
2020-11-06 10:39:42 -0300

    Creating the new class 'Cliente'

--- src/Conta.java
+++ src/Conta.java
@@ -4,7 +4,7 @@ public class Conta {
 	double saldo; //valor padrão dessas variáveis é zero, diferente de uma variável dentro da main class que não tem valor algum;
 	int agencia;
 	int numero;
-	String titular;
+	Cliente titular;
 	
 	
 	public void deposita(double valor) { //o deposita é o método, enquanto o valor é o parâmetro. O void é usado uma vez que não há retorno 
@@ -12,5 +12,20 @@ public class Conta {
 		this.saldo = this.saldo+valor; //saldo é um atributo e valor é uma variável
 	}
 	
+	public boolean saca(double valor){
+		if(this.saldo >= valor) {
+			this.saldo = this.saldo-valor;
+			return true;
+		}
+		return false;			
+	}
 	
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saca(valor);
+			destino.deposita(valor);
+			return true;
+		}
+		return false;
+	}
 }
--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -3,11 +3,45 @@ public class TestaMetodo {
 	
 	public static void main(String[] args) {
 		
+		Cliente fulana = new Cliente();
+		fulana.nome = "Fulana";
+		fulana.cpf = "111.111.111-11";
+		fulana.profissao = "Analista";
 		Conta contaFulana = new Conta();
 		contaFulana.saldo = 100;
-		contaFulana.deposita(200);
-		//o método depoista() altera o saldo, que é uma especificação de Conta;
-		System.out.println("O saldo da conta da Fulana é "+contaFulana.saldo);
+		contaFulana.titular = fulana;
+		
+		
+		Cliente beltrana = new Cliente();
+		beltrana.nome = "Beltrana";
+		beltrana.cpf = "222.222.222-22";
+		beltrana.profissao = "Contadora";
+		Conta contaBeltrana = new Conta();
+		contaBeltrana.saldo = 1000;
+		contaBeltrana.titular = beltrana;
+		
+		
+		
+		//o método deposita() altera o saldo, que é uma especificação de Conta;
+		contaFulana.deposita(200);		
+		//System.out.println("O saldo da conta da Fulana é "+contaFulana.saldo);
+		
+		//chamando o método saca
+		if(contaFulana.saca(100)) {
+			System.out.println("Saque feito com sucesso! Saldo atual da "+contaFulana.titular.nome+" é "+contaFulana.saldo);
+		} else {
+			System.out.println("Saldo insuficiente");
+		}		
+				
+		//chamando o método transfere
+		if(contaBeltrana.transfere(300, contaFulana)) {
+			System.out.println("Transferência feita com sucesso!");
+			System.out.println("O saldo da conta da "+contaFulana.titular.nome+" agora é "+contaFulana.saldo);
+			System.out.println("O saldo da conta da "+contaBeltrana.titular.nome+" agora é "+contaBeltrana.saldo);
+		}else {
+			System.out.println("Saldo insuficiente");
+		}
+
 	}
 
 }
